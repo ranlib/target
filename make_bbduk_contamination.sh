eefcaaca005663a266cc3f43fe42922d39d3cd56
Dieter Best <dieterbest@gmail.com>
2023-08-30 00:32:20 -0700

    make_bbduk_contamination.sh: add creation of a small contamination database

--- make_bbduk_contamination.sh
+++ make_bbduk_contamination.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-tar czvf bbduk_contamination.tar.gz \
+tar czvf  contamination/bbduk_contamination.tar.gz \
     contamination/Escherichia_coli_gca_001606525.ASM160652v1.dna.toplevel.fa.gz
+
+tar czvf  contamination/bbduk_contamination_polyA.tar.gz \
+    contamination/polyA.fa.gz
